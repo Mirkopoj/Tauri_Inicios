f58f2db0774cea9f3253dbad3a474f4c4908cdb3
Mirkopoj <mirkopoj@hotmail.com>
2023-05-26 00:50:51 -0300

    Primer commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,31 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::process::Command;
+use std::str;
+
+fn main() {
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![greet, ls])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+   format!("Holii, {}!", name)
+}
+
+#[tauri::command]
+fn ls(args: [&str;2]) -> String {
+    str::from_utf8(
+        &Command::new("ls")
+                .args(&args)
+                .output()
+                .expect("ou nou fallou")
+                .stdout
+            )
+        .expect("Fallo from_utf8")
+        .to_string()
+}
+
