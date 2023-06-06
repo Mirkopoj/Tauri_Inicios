a64fb58558b1faf2a24d7695964ca3c2036ad16e
Lari <lara.a.torletti@gmail.com>
2023-06-06 13:42:07 -0300

    con pltly

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,10 +3,14 @@
 
 use std::process::Command;
 use std::str;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet, ls])
+    .invoke_handler(tauri::generate_handler![greet, ls, button, button1])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
@@ -38,4 +42,29 @@ impl ToHTML for String {
     fn to_html(&self) -> String {
         self.replace("\n", "<br>")
     }
+}
+
+#[tauri::command]
+fn button() -> String {
+    format!("Apretaste el botón!!")}
+
+#[tauri::command]
+fn button1() -> String {
+    // Create a path to the desired file
+    let path = Path::new("hello.txt");
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("{} contains:\n{}", display, s),
+    }
+    s
 }
